577e32be7a19f3a6801f359e79ac39f01a65a5d9
Steve McLeod <steve.mcleod@gmail.com>
2019-01-20 15:39:56 +0100

    Added game state

--- src/main/java/com/barbarysoftware/whatworld/Canvas.java
+++ src/main/java/com/barbarysoftware/whatworld/Canvas.java
@@ -12,7 +12,7 @@ public class Canvas extends JPanel {
     private static final int HEIGHT = 600;
 
     private int clickCount = 0;
-    private boolean gameOver = false;
+    private GameState gameState = GameState.WAITING_TO_START;
 
     private PlayerSprite playerSprite = new PlayerSprite(WIDTH / 2, HEIGHT / 2);
 
@@ -22,17 +22,26 @@ public class Canvas extends JPanel {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                if (gameOver) {
-                    clickCount = 0;
-                    gameOver = false;
-                } else {
-                    moveMe(e.getX(), e.getY());
-                    clickCount++;
-                    if (clickCount >= 10) {
-                        gameOver = true;
-                    }
-                }
+                switch (gameState) {
 
+                    case WAITING_TO_START:
+                        clickCount = 0;
+                        gameState = GameState.PLAYING;
+                        break;
+
+                    case PLAYING:
+                        moveMe(e.getX(), e.getY());
+                        clickCount++;
+                        if (clickCount >= 10) {
+                            gameState = GameState.GAME_OVER;
+                        }
+                        break;
+
+                    case GAME_OVER:
+                        clickCount = 0;
+                        gameState = GameState.PLAYING;
+                        break;
+                }
                 repaint();
 
             }
@@ -51,25 +60,50 @@ public class Canvas extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
+
         g.setColor(Color.DARK_GRAY);
         g.fillRect(0, 0, getWidth(), getHeight());
+
+        switch (gameState) {
+            case WAITING_TO_START:
+                doWaitingToStartPaint(g);
+                break;
+            case PLAYING:
+                doPlayingPaint(g);
+                break;
+            case GAME_OVER:
+                doGameOverPaint(g);
+                break;
+        }
+
+    }
+
+    private void doWaitingToStartPaint(Graphics g) {
+        drawInstructions(g, "Welcome to What World", "Click anywhere to start");
+    }
+
+    private void drawInstructions(Graphics g, String text, String subtext) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("SansSerif", Font.PLAIN, 40));
+        int sw = g.getFontMetrics().stringWidth(text);
+        int sh = g.getFontMetrics().getHeight();
+        g.drawString(text, (getWidth() - sw) / 2, (getHeight() - sh) / 2);
+
+        g.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        int sw2 = g.getFontMetrics().stringWidth(subtext);
+        g.drawString(subtext, (getWidth() - sw2) / 2, (int) ((getHeight() - sh) / 2 + sh * 1.2));
+    }
+
+    private void doPlayingPaint(Graphics g) {
         g.setColor(Color.WHITE);
         String str;
-        if (gameOver) {
-            str = "You won! Congratulations. Click anywhere to restart.";
-
-        } else {
-            if (clickCount > 0) {
-                str = "Score: " + NumberFormat.getNumberInstance().format(clickCount * 1000);
-            } else {
-                str = "Welcome to What World.";
-
-            }
-
-        }
+        str = "Score: " + NumberFormat.getNumberInstance().format(clickCount * 1000);
         g.drawString(str, 20, 20);
         playerSprite.paint(g);
+    }
 
+    private void doGameOverPaint(Graphics g) {
+        drawInstructions(g, "You won! Congratulations.", "Click anywhere to restart");
     }
 
 }
---
+++ src/main/java/com/barbarysoftware/whatworld/GameState.java
@@ -0,0 +1,7 @@
+package com.barbarysoftware.whatworld;
+
+public enum GameState {
+    WAITING_TO_START,
+    PLAYING,
+    GAME_OVER
+}
